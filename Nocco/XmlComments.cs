0994dd7676d15690d674d6af301073de2accc679
Will Pearson <pearswj@gmail.com>
2014-02-04 21:25:23 +0000

    wip xml parsing

---
+++ Nocco/XmlComments.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Nocco
+{
+    public class XmlComments
+    {
+        public XmlComments(string input)
+        {
+            this.Input = input;
+            var clean = "<wrapper>" + this.Input.Replace("///", "") + "</wrapper>";
+            this.Xml = XDocument.Parse(clean);
+        }
+
+        public string Input
+        {
+            get;
+            private set;
+        }
+
+        public XDocument Xml
+        {
+            get;
+            private set;
+        }
+    }
+}
---
+++ NoccoTests/XmlCommentsTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using NUnit.Framework;
+using Nocco;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace NoccoTests
+{
+    [TestFixture]
+    public class XmlCommentsTest
+    {
+        [Test]
+        public void CanParseXML()
+        {
+            var xml = @"/// <summary>
+/// <para>Split a face into two faces by inserting a new edge</para>
+/// <seealso cref=""MergeFaces""/>
+/// </summary>
+/// <param name=""to"">The index of a second halfedge adjacent to the face to split.
+/// The new edge will end at the start of this halfedge.</param>
+/// <param name=""from"">The index of a halfedge adjacent to the face to split.
+/// The new edge will begin at the start of this halfedge.</param>
+/// <returns>The index of one of the newly created halfedges, or -1 on failure.
+/// The returned halfedge will be adjacent to the pre-existing face.</returns>";
+
+            var SUT = new XmlComments(xml);
+
+            Assert.AreEqual(xml, SUT.Input);
+
+            var ret = "The index of one of the newly created halfedges, or -1 on failure. The returned halfedge will be adjacent to the pre-existing face.";
+
+            foreach (var item in SUT.Xml.Descendants("returns"))
+            {
+                System.Console.WriteLine(item);
+            }
+        }
+    }
+}
